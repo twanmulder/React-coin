98a9c04290c23d2362b9a1cf3eaf8c948826ada2
twanmulder <twan.mulder@gmail.com>
2019-03-05 13:55:08 +0100

    Deleted old data
    Deleted old data

--- stages/10/src/components/common/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Loading.css';
-
-const Loading = () => {
-  return <div className="Loading" />;
-}
-
-export default Loading;
--- stages/final/src/config.js
+++
@@ -1,4 +0,0 @@
-/**
- * API url
- */
-export const API_URL = 'https://api.udilia.com/coins/v1';
